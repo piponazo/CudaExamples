0522fb232d4702aa26178df71017220e9bc33f47
Luis Díaz Más <piponazo@gmail.com>
2019-04-25 22:52:40 +0200

    Vector addition CUDA version

---
+++ apps/chapter02/cuda_vectorAddition.cu
@@ -0,0 +1,77 @@
+#include <gflags/gflags.h>
+
+#include <chrono>
+#include <cmath>
+#include <iostream>
+#include <random>
+#include <vector>
+
+#include <cuda.h>
+#include <cuda_runtime_api.h>
+
+DEFINE_int32(N, 1000, "Number of elements in vectors");
+
+__global__ void vecAddKernel(const float *a, const float *b, const int n, float *c)
+{
+    int i = threadIdx.x + blockDim.x * blockIdx.x;
+    if (i < n) {
+        c[i] = a[i] + b[i];
+    }
+}
+
+void inputInitialization(float *h_A, float *h_B, const int n)
+{
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<float> dis(0.0f, 100.0f);
+
+    for (int i = 0; i < n; i++) {
+        h_A[i] = dis(gen);
+        h_B[i] = dis(gen);
+    }
+}
+
+void vecAdd(const float *h_A, const float *h_B, const int n, float *h_C)
+{
+    const int size = n * sizeof(float);
+    float *d_A, *d_B, *d_C;
+
+    cudaMalloc(&d_A, size);
+    cudaMalloc(&d_B, size);
+    cudaMalloc(&d_C, size);
+
+    cudaMemcpy(d_A, h_A, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, h_B, size, cudaMemcpyHostToDevice);
+
+    vecAddKernel<<<std::ceil(n / 256.0), 256>>>(d_A, d_B, n, d_C);
+
+    cudaMemcpy(h_C, d_C, size, cudaMemcpyDeviceToHost);
+    cudaFree(d_A);
+    cudaFree(d_B);
+    cudaFree(d_C);
+}
+
+void verificationHost(const float *h_A, const float *h_B, const float *h_C, const int n)
+{
+    for (int i = 0; i < n; ++i) {
+        if (fabs(h_A[i] + h_B[i] - h_C[i]) > 1e-5) {
+            fprintf(stderr, "Result verification failed at element %d!\n", i);
+            exit(EXIT_FAILURE);
+        }
+    }
+}
+
+int main(int argc, char **argv)
+{
+    auto start = std::chrono::steady_clock::now();
+
+    std::vector<float> h_A(FLAGS_N), h_B(FLAGS_N), h_C(FLAGS_N);
+    inputInitialization(h_A.data(), h_B.data(), FLAGS_N);
+    vecAdd(h_A.data(), h_B.data(), FLAGS_N, h_C.data());
+    verificationHost(h_A.data(), h_B.data(), h_C.data(), FLAGS_N);
+    auto end = std::chrono::steady_clock::now();
+    std::cout << "Total time: " << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count()
+              << " us\n";
+
+    return EXIT_SUCCESS;
+}
--- cmake_stuff/compileroptions.cmake
+++ cmake_stuff/compileroptions.cmake
@@ -1,9 +1,16 @@
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -pedantic -Wextra")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wextra")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Werror")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Werror=return-type")
-
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fvisibility=hidden") # All the symbols will be hidden by default.
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wreturn-type")
+
+
+#set(CMAKE_CUDA_FLAGS "${CMAKE_CUDA_FLAGS} -Wextra")
+#set(CMAKE_CUDA_FLAGS "${CMAKE_CUDA_FLAGS} -Werror")
+#set(CMAKE_CUDA_FLAGS "${CMAKE_CUDA_FLAGS} -Wall -Werror=return-type")
+#set(CMAKE_CUDA_FLAGS "${CMAKE_CUDA_FLAGS} -fvisibility=hidden") # All the symbols will be hidden by default.
+#set(CMAKE_CUDA_FLAGS "${CMAKE_CUDA_FLAGS} -Wreturn-type")
+
 #set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-long-long")
 #set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-maybe-uninitialized")
 
